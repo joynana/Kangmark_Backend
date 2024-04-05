e4f01299663d925d00c97695083f0025bf3a406f
goeun <goeunna2@gmail.com>
2024-04-05 16:17:36 +0900

    fine

--- main/admin.py
+++ main/admin.py
@@ -3,3 +3,8 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Courses)
+admin.site.register(User)
+admin.site.register(Categories)
+admin.site.register(Courses_list)
+admin.site.register(Schedule)
+admin.site.register(KangMark)
